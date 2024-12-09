eb5b5eb620b4a197ea4354d9fc234a0ab564de85
dksqjq4851 <dksqjq4851@naver.com>
2024-12-09 18:48:08 +0900

    java 4일차

--- src/C01Basic/C07Array.java
+++ src/C01Basic/C07Array.java
@@ -131,22 +131,141 @@ public class C07Array {
 
 //        문자정렬
 //        아스코코드(unicode)기준으로 정렬
-        String[] fruits = {"banana","apple","cherry"};
-//        오름차순
-        Arrays.sort(fruits);
-        System.out.println(Arrays.toString(fruits));
-//        내림차순 : 기본형타입은 Comparator 로 처리 불가
-        Arrays.sort(fruits, Comparator.reverseOrder());
-        int[] arr = {5,1,2,3,6};
-//        Arrays.sort(arr, Comparator.reverseOrder());
-        Arrays.sort(arr);
-//        k번째수 프로그래머스
+//        String[] fruits = {"banana","apple","cherry"};
+////        오름차순
+//        Arrays.sort(fruits);
+//        System.out.println(Arrays.toString(fruits));
+////        내림차순 : 기본형타입은 Comparator 로 처리 불가
+//        Arrays.sort(fruits, Comparator.reverseOrder());
+//        int[] arr = {5,1,2,3,6};
+////        Arrays.sort(arr, Comparator.reverseOrder());
+//        Arrays.sort(arr);
+////        k번째수 프로그래머스
+//
+//
+//
+//
+//        System.out.println(Arrays.toString(fruits));
+//        String[] fruits2 = {"apple","applee","applef"};
+//        숫자 조합의 합
+//        모두 각기 다른 숫자의 배열이 있을때, 만들어질수 있는 2개의 조합의 합을 출력
+//        int[] intArr = {10, 20, 30, 40, 50, 60};
+//        int[] arr1 = new int[intArr.length* intArr.length];
+//        int index = 0;
+//        for(int i=0;i<intArr.length-1;i++){
+//            for(int j = i+1;j<intArr.length;j++){
+//                arr1[index] = intArr[i]+intArr[j];
+//                index++;
+//            }
+//        }
+//        int[] arr2 = new int[index];
+//        for(int i = 0; i<index; i++){
+//            arr2[i] = arr1[i];
+//        }
+//        Arrays.sort(arr2);
+//        System.out.println(Arrays.toString(arr2));
+//        int index2 = 0;
+//        int[] arr3 = new int[arr2.length];
+//        for(int i =0; i<arr2.length-1; i++){
+//            if(arr2[i]!=arr2[i+1]) {
+//                arr3[index2] = arr2[i];
+//                index2++;
+//            }
+//        }
+//        arr3[index2] = arr2[arr2.length-1];
+//        int[] answer =  new int[index2];
+//        for(int i = 0; i<index2;i++){
+//            answer[i] = arr3[i];
+//        }
+//        System.out.println(Arrays.toString(arr3));
+//        System.out.println(Arrays.toString(answer));
+//
+////        배열의복사
+////        Arrays.copyOf(배열명, length), Arrays.copyOfRange(배열명, start, end)
+//        int[] answer1 = Arrays.copyOf(arr3, index2);
+//        System.out.println(Arrays.toString(answer1));
+//        int[] answer2 = Arrays.copyOfRange(arr3, 0, index2);
+
+//        두 개 뽑아서 더하기
+//        int[] arr = {5,3,1,8,7};
+//        int target = 8;
+//        int index = 0;
+//        for (int i = 0; i <arr.length;i++){
+//            if(arr[i]==target){
+//                index++;
+//                break;
+//            }
+//            index++;
+//
+//        }
+//        System.out.println(index);
+
+//        이진검색(binary search) - 이분탐색
+//        사전에 오름차순정렬이 되어 있어야 이진검색 가능
+//        수 찾기
+//        int[] arr = {1,3,4,8,9,15,15};
+//        System.out.println(Arrays.binarySearch(arr, 18));
+
+////        배열간 비교
+//        int[] arr1 = {10,20,30};
+//        int[] arr2 = {10,20,30};
+//        System.out.println(arr1==arr2);
+////        Arrays.equals : 값과 순서까지 동일해야 true
+//        System.out.println(Arrays.equals(arr1, arr2));
+
+//        2차원배열의 선언과 할당
+//        int[][] arr = new int[2][3];
+//        arr[0][0] = 10;
+//        arr[0][1] = 20;
+//        arr[0][2] = 30;
+//        arr[1][0] = 40;
+//        arr[1][1] = 50;
+//        arr[1][2] = 60;
+//        int[][] arr2 = {{10,20,30},{40,50,60}};
+
+//        가변배열 선언 후 할당
+//        int[][]arr1 = new int[2][];
+//        arr1[0] = new int[2];
+//        arr1[1] = new int[3];
+//        arr1[0][0] = 10;
+//        arr1[0][1] = 20;
+//        arr1[1][0] = 30;
+//        arr1[1][1] = 40;
+//        arr1[1][2] = 50;
+////        가변배열 리터럴 방식;
+//        int[][]arr2 = {{10,20},{30,40,50}};
+
+//        [3][4]사이즈 배열 선언하고
+//        1~12까지 숫자값을 각 배열에 순차적으로 할당
+//        {1,2,3,4,},{5,6,7,8,}....
+//        int[][]arr = new int[3][4];
+//        int index = 0;
+//        for (int i=0;i<arr.length;i++){
+//            for(int j =0;j<arr[0].length;j++){
+//                arr[i][j] = j + 1+ index;
+//
+//            }
+//            index += arr.length+1;
+//        }
+        int[][]arr = new int[3][4];
+        int num = 1;
+        for (int i = 0; i<arr.length; i++){
+            for (int j=0;j<arr[i].length;j++){
+                arr[i][j] = num;
+                num++;
+            }
+        }
 
 
 
 
-        System.out.println(Arrays.toString(fruits));
-        String[] fruits2 = {"apple","applee","applef"};
+        System.out.println(Arrays.deepToString(arr));
+
+//        행렬의 덧셈 - 프로그래머스
+
+
+
+
 
 
     }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Pattern;
 
 public class main {
@@ -49,20 +47,123 @@ public class main {
 //        [1,5,2,6,3,7,4]
 
 //        List<int[]> list1 = new ArrayList<>();
-        int[] array = new int[]{1,5,2,6,3,7,4};
-        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
-        int[] answer = new int[commands.length];
+//        int[] array = new int[]{1,5,2,6,3,7,4};
+//        int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+//        int[] answer = new int[commands.length];
+//
+//        for(int i = 0;i<commands.length;i++){
+//            int[] temp = new int[commands[i][1]-commands[i][0]+1];
+//
+//            for(int j=commands[i][0];j<=commands[i][1];j++){
+//                temp[j-commands[i][0]] = array[j-1];
+//            }
+//            Arrays.sort(temp);
+//            answer[i] += temp[commands[i][2]-1];
+//
+//        }
+//        System.out.println(Arrays.toString(answer));
 
-        for(int i = 0;i<commands.length;i++){
-            int[] temp = new int[commands[i][1]-commands[i][0]+1];
+//        int[] numbers = {2,1,3,4,1};
+//        int index = 0;
+//        int[] arr1 = new int[numbers.length*numbers.length];
+//        for(int i = 0; i < numbers.length-1; i++){
+//            for (int j= i+1; j<numbers.length;j++){
+//                arr1[index] = numbers[i] + numbers[j];
+//                index++;
+//            }
+//        }
+//        int[] arr2 = Arrays.copyOf(arr1, index);
+//        Arrays.sort(arr2);
+//        int[] arr3 = new int[arr2.length];
+//        int index2 = 0;
+//        for(int i = 0; i<arr2.length;i++){
+//            if(i==arr2.length-1 || arr2[i]!=arr2[i+1]){
+//                arr3[index2] = arr2[i];
+//                index2++;
+//            }
+//        }
+//        int[] answer = Arrays.copyOf(arr3, index2);
+//        int[][] arr1 = {{1,2},{2,3}};
+//        int[][] arr2 = {{3,4},{5,6}};
+//        int[][] answer = new int[arr1.length][arr1[0].length];
+//        if(arr1.length==arr2.length&&arr1[0].length==arr2[0].length){
+//            for(int i =0;i<arr1.length;i++){
+//                for(int j = 0;j<arr1[0].length;j++){
+//                    answer[i][j] = arr1[i][j] + arr2[i][j];
+//                }
+//            }
+//
+//        }
+//        System.out.println(Arrays.deepToString(answer));
+//        int index = 0;
+//        int n = 3;
+//        int[] numlist = {4,5,6,7,8,9,10,11,12};
+//        List<Integer> myList = new ArrayList<>();
+//        for(int a : numlist){
+//            if((a % n) == 0){
+//                myList.add(a);
+//            }
+//        }
+//
+//        System.out.println(myList);
+//        int[] answer = new int[myList.size()];
+//        for(int i= 0; i< answer.length;i++){
+//            answer[i] = myList.get(i);
+//        }
+//        System.out.println(Arrays.toString(answer));
 
-            for(int j=commands[i][0];j<=commands[i][1];j++){
-                temp[j-commands[i][0]] = array[j-1];
-            }
-            Arrays.sort(temp);
-            answer[i] += temp[commands[i][2]-1];
 
+//        String[] completion = {"nikola","filipa","marina","vinko"};
+//        String[] participant = {"marina","vinko","nikola","vinko","filipa"};
+//        String answer = "";
+//        boolean same = true;
+//
+//
+//        for(int i = 0; i <participant.length-1;i++){
+//            for (int j = i+1;j< participant.length;j++ ){
+//                if(participant[i].equals(j)){
+//                    answer = participant[j];
+//                    same = false;
+//
+//                }
+//            }
+//        }
+//        System.out.println(answer);
+//
+//        if(same) {
+//            for (int i = 0; i < participant.length; i++) {
+//                int index = 0;
+//                for (int j = 0; j < completion.length; j++) {
+//                    if (participant[i].equals(completion[j])) {
+//                        index = 1;
+//                        break;
+//                    }
+//                }
+//                if (index == 0) {
+//                    answer = participant[i];
+//                    break;
+//                }
+//            }
+//            System.out.println(answer);
+
+
+//        }
+        String answer = "";
+        String[] completion ={"stanko", "ana", "mislav"};
+        String[] participant ={"mislav", "stanko", "bye", "ana"};
+        Map<String ,Integer > map2 = new HashMap<>();
+        for(int i = 0; i<participant.length;i++){
+            map2.put(participant[i], map2.getOrDefault(participant[i], 0)+1);
         }
-        System.out.println(Arrays.toString(answer));
+        for(int i = 0; i<completion.length;i++){
+            map2.put(completion[i], map2.get(completion[i])-1);
+        }
+        for(int i = 0; i<map2.size();i++){
+            if(map2.get(participant[i])==1){
+                answer = participant[i];
+            }else if(map2.get(participant[i])==-1){
+                answer = participant[i];
+            }
+        }
     }
 }
